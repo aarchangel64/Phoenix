23d634e25fff04ed54769f59861f2ea0a9c174ef
Shrey Pasricha <shrey.pasricha@gmail.com>
2019-09-08 01:39:45 -0400

    Added some drone functions and changed to be an object

--- sensordata.py
+++ sensordata.py
@@ -1,53 +1,36 @@
 import time, sys
-import ps_drone  
-import json                                   # Import PS-Drone-API
+import ps_drone
+import json  # Import PS-Drone-API
 import math
 
-#drone reset, sensor config 
+# drone reset, sensor config
 
-drone = ps_drone.Drone()                           # Start using drone
-drone.startup()                                    # Connects to drone and starts subprocesses
+drone = ps_drone.Drone()  # Start using drone
+drone.startup()  # Connects to drone and starts subprocesses
 
-drone.reset()                                      # Sets drone's status to good
-while (drone.getBattery()[0]==-1): time.sleep(0.1) # Wait until drone has done its reset
-print "Battery: "+str(drone.getBattery()[0])+"% "+str(drone.getBattery()[1]) # Battery-status
-drone.useDemoMode(False)                           # Give me everything...fast
-drone.getNDpackage(["demo","pressure_raw","altitude","magneto","wifi"]) # Packets to decoded
-time.sleep(0.5)  
-#reset #idk wat this does it was in the documentation   
-drone.trim() 
+drone.reset()  # Sets drone's status to good
+while (drone.getBattery()[0] == -1): time.sleep(0.1)  # Wait until drone has done its reset
+print "Battery: " + str(drone.getBattery()[0]) + "% " + str(drone.getBattery()[1])  # Battery-status
+drone.useDemoMode(False)  # Give me everything...fast
+drone.getNDpackage(["demo", "altitude"])  # Packets to decoded
+time.sleep(0.5)
+# reset #idk wat this does it was in the documentation
+drone.trim()
 
-#main
+# main
 
 
 NDC = drone.NavDataCount
 end = False
 while not end:
-    while drone.NavDataCount==NDC:  time.sleep(0.001) # Wait until next time-unit
-    if drone.getKey():              end = True        # Stop if any key is pressed
-    NDC=drone.NavDataCount
-    navData = drone.NavData
-    velocity = navData["demo"][4] # Get velocity vector
-    speed = math.sqrt(velocity[0]**2 + velocity[1]**2 + velocity[2]**2)
-    print "-----------"
+	while drone.NavDataCount == NDC:  time.sleep(0.001)  # Wait until next time-unit
+	if drone.getKey():              end = True  # Stop if any key is pressed
+	NDC = drone.NavDataCount
+	navData = drone.NavData
+	velocity = navData["demo"][4]  # Get velocity vector
+	speed = math.sqrt(velocity[0] ** 2 + velocity[1] ** 2 + velocity[2] ** 2)
+	print "-----------"
 
-    print(json.dumps("Altitude: " + str(navData["altitude"][3])))
-    print(json.dumps("Battery: " + str(drone.getBattery()[0]))) #battery percentage, if battery is too low or not
-    print(json.dumps("Speed: " + str(speed)))
-
-
-    
-
-
-    
-
-
-
-
-
-
-
-
-   
-
-
+	print(json.dumps("Altitude: " + str(navData["altitude"][3])))
+	print(json.dumps("Battery: " + str(drone.getBattery()[0])))  # battery percentage, if battery is too low or not
+	print(json.dumps("Speed: " + str(speed)))
--- webapp/drone.py
+++ webapp/drone.py
@@ -1,25 +1,44 @@
 import ps_drone
 import time
 
-#drone = ps_drone.Drone()
 
-def connect():
-	time.sleep(1)
+class Drone:
+	def __init__(self):
+		self.drone = ps_drone.Drone()
+		self.connected = False
 
-def liftOff():
-	time.sleep(1)
+	def setup(self):
+		# drone reset, sensor config
+		self.drone.startup()  # Connects to drone and starts subprocesses
 
-def findFire():
-	time.sleep(1)
+		self.drone.reset()  # Sets drone's status to good
+		while self.drone.getBattery()[0] == -1: time.sleep(0.1)  # Wait until drone has done its reset
+		print("Battery: " + str(self.drone.getBattery()[0]) + "% " + str(self.drone.getBattery()[1]))  # Battery-status
+		self.drone.useDemoMode(True)
+		self.drone.getNDpackage(["demo", "altitude"])  # Packets to decoded
+		time.sleep(0.5)
+		# reset #idk wat this does it was in the documentation
+		self.drone.trim()
+		self.connected = True
 
-def fightFire():
-	time.sleep(1)
+	def connect(self):
+		if not self.connected:
+			self.setup()
 
-def land():
-	time.sleep(1)
+	def liftOff(self):
+		self.drone.takeoff()
 
-def fire():
-	drone.startup()
-	drone.takeoff()
-	time.sleep(7.5)
-	drone.land()
+	def findFire(self):
+		time.sleep(1)
+
+	def fightFire(self):
+		time.sleep(1)
+
+	def land(self):
+		self.drone.land()
+
+	def fire(self):
+		self.drone.startup()
+		self.drone.takeoff()
+		time.sleep(7.5)
+		self.drone.land()
