7bbf11089ae236ed6c2bb82495cab05a0b8fe3e9
Shrey Pasricha <shrey.pasricha@gmail.com>
2019-09-07 23:36:03 -0400

    use service account json

--- google cloud/phoenix-ml.py
+++ google cloud/phoenix-ml.py
@@ -1,19 +1,18 @@
 from google.cloud import vision
 from google.cloud.vision import types
+import cv2
 
 
 def fire_vision(input_image):
-    client = vision.ImageAnnotatorClient()
 
-    #TODO: figure out what format we are recieving image data in and covert it to this
-    image = types.Image(content=input_image)
+	client = vision.ImageAnnotatorClient().from_service_account_json('res/account.json')
+	image = client.image(content=cv2.imencode('.jpg', input_image)[1].tostring())
 
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
+	response = client.label_detection(image=image)
+	labels = response.label_annotations
 
-    print('Labels:')
-    for label in labels:
-        print(label.description)
-		if (label.description == fire)
-			moveForward()
-
+	print('Labels:')
+	for label in labels:
+		print(label.description)
+		if (label.description == fire):
+			print("test")
