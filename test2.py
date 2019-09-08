63dde149880a61b3647aadade9ff1fb9bc219371
Jaclyn Chen <jaclyn.chen5@gmail.com>
2019-09-08 00:09:00 -0400

    sensor data

--- test2.py
+++ test2.py
@@ -1,4 +1,4 @@
-import time
+'''import time
 import ps_drone                # Imports the PS-Drone-API
 
 drone = ps_drone.Drone()       # Initializes the PS-Drone-API
@@ -20,7 +20,7 @@ if key == "0":
 
 	drone.land()                   # Drone lands
 
-'''
+
 
 drone.moveup(0.5)      
 time.sleep(2)               # Gives the drone time to start
@@ -41,6 +41,36 @@ drone.land()                   # Drone lands
 
 '''
 
+
+
+import time
+import ps_drone           # Imports the PS-Drone-API
+
+drone = ps_drone.Drone()  # Initializes the PS-Drone-API
+drone.startup()           # Connects to the drone and starts subprocesses
+
+drone.takeoff()           # Drone starts
+time.sleep(7.5)           # Gives the drone time to start
+
+drone.moveForward()       # Drone flies forward...
+time.sleep(2)             # ... for two seconds
+drone.stop()              # Drone stops...
+time.sleep(2)             # ... needs, like a car, time to stop
+
+drone.moveBackward(0.25)  # Drone flies backward with a quarter speed...
+time.sleep(1.5)           # ... for one and a half seconds
+drone.stop()              # Drone stops
+time.sleep(2)
+
+drone.setSpeed(1.0)       # Sets default moving speed (from 0.2 = 20%) to 1.0 (=100%)
+print drone.setSpeed()    # Shows the default moving speed
+
+drone.turnLeft()          # Drone moves full speed to the left...
+time.sleep(2)             # ... for two seconds
+drone.stop()              # Drone stops
+time.sleep(2)
+
+drone.land()              # Drone lands
 
 
 
